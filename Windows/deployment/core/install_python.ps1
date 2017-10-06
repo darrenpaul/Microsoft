724380512f1e9f2e672d163c1a89e2105bdf6557
Darren Paul <darrenpaul@users.noreply.github.com>
2017-10-06 08:39:38 +0200

    Started on downloading python with powershell

--- Windows/deployment/core/install_python.ps1
+++ Windows/deployment/core/install_python.ps1
@@ -1,13 +1,31 @@
+#if (Test-Path $path) {
+ #   throw "File already exists."
+#} else {
 
-
-
+   # The thing you really wanted to do.
+#}
 
 
 $script_root = $PSScriptRoot
-Get-Content "path-to-install_python-config"
+$config = $script_root + "\config\install_python.txt"
 
-if (Test-Path $path) {
-    throw "File already exists."
-} else {
-   # The thing you really wanted to do.
+$install_directory_pattern = "^(.*)install_directory = (.*)$"
+$download_url_pattern = "[^download_url = ]"
+$downloaded_path_pattern = "[^downloaded_path = ]"
+
+$install_directory = ""
+$download_url = ""
+$downloaded_path = ""
+
+foreach($line in Get-Content $config) {
+    if($line -match $install_directory_pattern){
+        $var = $line -match $install_directory_pattern
+        Write-Host $var
+    }
 }
+
+#if (Test-Path $path) {
+#    throw "File already exists."
+#} else {
+#   # The thing you really wanted to do.
+#}
