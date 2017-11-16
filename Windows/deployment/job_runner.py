0c1ea443e0928844fa3c94c2d8da7aec3169ad60
Darren Paul <darrenpaul@users.noreply.github.com>
2017-11-16 17:04:09 +0200

    Added chrome installer

--- Windows/deployment/job_runner.py
+++ Windows/deployment/job_runner.py
@@ -1,8 +1,12 @@
 import sys
 import logging
+import json
 import re
 import argparse
 import os
+import platform
+from subprocess import Popen, PIPE, call
+from pprint import pprint
 
 def start_parser():
     # Main
@@ -15,9 +19,42 @@ def start_parser():
                         help='Client assetbuilds to update')
     args = parser.parse_args()
 
-    if args == "google":
-        sys.path.append()
+    job = args.job
+
+    if job == "google":
+        _parse_data()
+
+def _parse_data():
+    data = json.load(open("C:/dev/Microsoft/Windows/deployment/jobs/google_chrome.json"))
+
+    source = data["paths"]["source"]["local"]
+
+    command = "{} {}".format(_get_commands(data=data["command"]), source)
+
+    print "-" * 140
+    print "-" * 140
+    print "SOURCE", source
+    print "COMMAND", command
+    print "-" * 140
+    print "-" * 140
+
+    _run_command(command=command)
+
+def _get_commands(data=None):
+    command = ""
+    if data:
+        for key, val in data.iteritems():
+            command = "{} {}".format(command, val)
+
+    return command.strip()
+
+def _run_command(command=basestring):
+    powershell = "C:/WINDOWS/system32/WindowsPowerShell/v1.0/powershell.exe"
+    # command = "{} -Command Start-Process -FilePath msiexec -ArgumentList {} -Wait".format(powershell, command)
+    command = "powershell.exe Start-Process -FilePath msiexec -ArgumentList '{}' -Wait".format(command)
+    print command
+    Popen(command, shell=True)
+    # call(command)
 
 if __name__ == '__main__':
     start_parser()
-
